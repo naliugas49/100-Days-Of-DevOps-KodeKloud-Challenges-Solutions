124d9a8bc06a42025aec5ec6ce30b30303ee3a3c
Dairon <121972944+naliugas49@users.noreply.github.com>
2025-10-05 22:47:20 +0200

    Update comands.sh

--- Day 43: Docker Ports Mapping/comands.sh
+++ Day 43: Docker Ports Mapping/comands.sh
@@ -1,5 +1,5 @@
 # 1. Pull nginx:alpine image
-docker pull nginx:alpine
+docker pull nginx:alpine-perl
 
 # 2. Run container named cluster with port mapping
 docker run -d --name cluster -p 3001:80 nginx:alpine
