2966aded908d19abd4764dd32192cd454470b2da
Dairon <121972944+naliugas49@users.noreply.github.com>
2025-10-15 13:10:04 +0200

    Create comands.sh

---
+++ Day 54: Kubernetes Shared Volumes/comands.sh
@@ -0,0 +1,14 @@
+## Create deploy
+
+```vi share-datacenter.yaml````
+
+## Apply deploy
+````k apply -f share-datacenter.yaml```` 
+
+## Check
+k get pod
+
+k exec -it volume-share-datacenter -c volume-container-datacenter-1 -- sh
+
+
+k exec -it volume-share-datacenter -c volume-container-datacenter-2 -- sh
